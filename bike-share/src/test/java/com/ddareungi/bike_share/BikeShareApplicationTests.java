93f6ec932e66ce5d15840fe1d73f1ba22dd28081
kyunseuk <kyenseuk@gmail.com>
2025-05-25 19:31:11 +0900

    project simple setting files

---
+++ bike-share/src/test/java/com/ddareungi/bike_share/BikeShareApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ddareungi.bike_share;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BikeShareApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
